2f467198e02b5b5c01aa4d6e28c99f8a5ccd60ef
Feterson <feterson@bk.ru>
2024-02-11 21:05:01 +0600

    created todo controller

---
+++ src/todos/todo.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodo } from './dto/create-todo.dto';
+import { ChangeTodo } from './dto/change-todo.dto';
+
+@Controller('todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+  @Get()
+  getAllTodos() {
+    return this.todoService.findAll();
+  }
+  @Get(':id')
+  getOneTodo(@Param('id') id: string) {
+    return this.todoService.findOne(id);
+  }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-Type', 'application/json')
+  createTodo(@Body() createTodo: CreateTodo) {
+    return this.todoService.create(createTodo);
+  }
+  @Patch(':id')
+  changeTodo(@Param('id') id: string, @Body() changeTodo: ChangeTodo) {
+    return this.todoService.update(id, changeTodo);
+  }
+  @Delete(':id')
+  removeTodo(@Param('id') id: string) {
+    return this.todoService.remove(id);
+  }
+}
